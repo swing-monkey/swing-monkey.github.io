374564852edda8312090d3d9993bc2ee056366d2
buitrongtuanlc2012 <buitrongtuanlc2012@gmail.com>
2023-11-02 11:22:30 +0700

    update

---
+++ js/full.js
@@ -0,0 +1,12 @@
+function open_fullscreen(){
+    let game = document.getElementById("game-area");
+	if (game.requestFullscreen) {
+	  game.requestFullscreen();
+	} else if (game.mozRequestFullScreen) { /* Firefox */
+	  game.mozRequestFullScreen();
+	} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+	  game.webkitRequestFullscreen();
+	} else if (game.msRequestFullscreen) { /* IE/Edge */
+	  game.msRequestFullscreen();
+	}
+}
